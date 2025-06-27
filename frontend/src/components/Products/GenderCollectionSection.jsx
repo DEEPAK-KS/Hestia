4f04fa8046beedf664a0815c95eb957729ac7586
DEEPAK-KS <deepakks2026@mca.ajce.in>
2025-06-27 19:15:29 +0530

    image path

--- frontend/src/components/Products/GenderCollectionSection.jsx
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import mensCollection from '../../assets/mens-Collection.webp'
+import mensCollection from '../../assets/mens-collection.webp'
 import womensCollection from '../../assets/womens-Collection.webp'
 import { Link } from 'react-router-dom'
 
