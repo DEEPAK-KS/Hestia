7fdf6c648e146680c30eac8ad09aca0fe3d7f671
DEEPAK-KS <107565652+DEEPAK-KS@users.noreply.github.com>
2025-06-27 20:00:59 +0530

    Update Topbar.jsx

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -8,15 +8,9 @@ const Topbar = () => {
     <div className='bg-[#ea2e0e] text-white'>
         <div className='container mx-auto flex justify-between items-center py-3 px-4'>
             <div className="hidden md:flex items-center space-x-4">
-                <a href="#" className='hover:text-gray-300'>
-                    <TbBrandMeta className="h-5 w-5"/>
-                </a>
-                <a href="#" className='hover:text-gray-300'>
-                    <IoLogoInstagram className="h-5 w-5"/>
-                </a>
-                <a href="#" className='hover:text-gray-300'>
-                    <RiTwitterXLine className="h-4 w-4"/>
-                </a>
+                <a href="https://www.facebook.com/deepak.ks.56614?mibextid=ZbWKwL" target='_blank' rel='noopener noreferrer' className='hover:text-gray-300'><TbBrandMeta className='h-5 w-5'/></a>
+                    <a href="https://www.instagram.com/deepak_ks_official/" target='_blank' rel='noopener noreferrer' className='hover:text-gray-300'><IoLogoInstagram className='h-5 w-5'/></a>
+                    <a href="https://www.linkedin.com/in/depak-ks/" target='_blank' rel='noopener noreferrer' className='hover:text-gray-300'><RiTwitterXLine className='h-4 w-4'/></a>
             </div>
             <div className="text-sm text-center flex-grow">
                 <span>We Ship World-Wide Fast & Reliable</span>
